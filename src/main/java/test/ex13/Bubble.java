2ef464b51586636a8ca9ae14a33e20d5af9258d9
sukkie <sukkie512@yahoo.co.jp>
2022-09-29 21:54:49 +0900

    clrerBubble

---
+++ src/main/java/test/ex13/Bubble.java
@@ -0,0 +1,149 @@
+package test.ex13;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.swing.*;
+
+@Getter
+@Setter
+public class Bubble extends JLabel implements Moveable {
+
+    // 의존성 컴포지트
+    private BubbleFrame mContext;
+    private Player player;
+
+    private BackgroundBubbleService backgroundBubbleService;
+
+    // 위치상태
+    private int x;
+    private int y;
+
+    // 움직임 상태
+    private boolean left;
+    private boolean right;
+    private boolean up;
+
+    // 적군을 맞춘 상태
+    // 0 : 물방울, 1 : 적을 가둔 물방울
+    private int state;
+
+    private ImageIcon bubble;   // 물방울
+    private ImageIcon bubbled;  //적을 가둔 상태
+    private ImageIcon bomb;     // 물방울이 터진상태
+
+    public Bubble(BubbleFrame mContext) {
+        this.mContext = mContext;
+        this.player = mContext.getPlayer();
+        initObject();
+        initSetting();
+        initThread();
+    }
+
+    private void initObject() {
+        bubble = new ImageIcon(BubbleFrame.loader.getResource("image/bubble.png"));
+        bubbled = new ImageIcon(BubbleFrame.loader.getResource("image/bubbled.png"));
+        bomb = new ImageIcon(BubbleFrame.loader.getResource("image/bomb.png"));
+
+        backgroundBubbleService = new BackgroundBubbleService(this);
+    }
+
+    private void initSetting() {
+        left = false;
+        right = false;
+        up = false;
+
+        x = player.getX();
+        y = player.getY();
+
+        setIcon(bubble);
+        setSize(50, 50);
+
+        state = 0;
+    }
+
+    private void initThread() {
+        // 버블은 쓰레드가 하나만 필요.
+        new Thread(() -> {
+            if (player.getPlayerWay() == PlayerWay.LEFT) {
+                left();
+            } else {
+                right();
+            }
+        }).start();
+    }
+
+    @Override
+    public void up() {
+        up = true;
+        while (up) {
+            y--;
+            setLocation(x, y);
+
+            if (backgroundBubbleService.topWall()) {
+                up = false;
+                break;
+            }
+            try {
+                Thread.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        clearBubble(); // 천장에 버블이 도착하고 나서 3초 후에 메모리에서 소멸
+    }
+
+    private void clearBubble() {
+        try {
+            Thread.sleep(3000);
+            setIcon(bomb);
+            Thread.sleep(500);
+            mContext.remove(this); // 버블프레의 버블이 메모리에서 소멸된다.
+            mContext.repaint(); // 버블프레임 전체를 다시 그린다.
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void right() {
+        right = true;
+        for (int i = 0; i < 400; i++) {
+            x++;
+            setLocation(x, y);
+
+            if (backgroundBubbleService.rightWall()) {
+                right = false;
+                break;
+            }
+
+            try {
+                Thread.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        up();
+    }
+
+    @Override
+    public void left() {
+        left = true;
+        for (int i = 0; i < 400; i++) {
+            x--;
+            setLocation(x, y);
+
+            if (backgroundBubbleService.leftWall()) {
+                left = false;
+                break;
+            }
+
+            try {
+                Thread.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        up();
+    }
+}
---
+++ src/main/java/test/ex13/Player.java
@@ -0,0 +1,142 @@
+package test.ex13;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.swing.*;
+
+@Setter
+@Getter
+public class Player extends JLabel implements Moveable {
+
+    // 위치상태
+    private int x;
+    private int y;
+
+    // 플레이어의 방향(방울 방향을 위해)
+    private PlayerWay playerWay;
+
+    // 움직임 상태
+    private boolean left;
+    private boolean right;
+    private boolean up;
+    private boolean down;
+
+    // 벽에 충돌한 상태
+    private boolean leftWallCrash;
+    private boolean rightWallCrash;
+
+    // 플레이어의 속도 상태
+    private final static int SPEED = 4;
+
+    private final static int JUMP_SPEED = 3;
+
+    private ImageIcon playerR, playerL;
+
+    public Player() {
+        initObject();
+        initSetting();
+        initBackgroundPlayerService();
+    }
+
+    private void initObject() {
+        playerR = new ImageIcon(BubbleFrame.loader.getResource("image/playerR.png"));
+        playerL = new ImageIcon(BubbleFrame.loader.getResource("image/playerL.png"));
+    }
+
+    private void initSetting() {
+        x = 80;
+        y = 540;
+
+        up = false;
+        down = false;
+        left = false;
+        right = false;
+
+        leftWallCrash = false;
+        rightWallCrash = false;
+
+        playerWay = PlayerWay.RIGHT;
+        setIcon(playerR);
+        setSize(50, 50);
+        setLocation(x, y);
+    }
+
+    private void initBackgroundPlayerService() {
+        new Thread(new BackgroundPlayerService(this)).start();
+    }
+
+    // left + up, right + up
+    @Override
+    public void up() {
+        up = true;
+        new Thread(() -> {
+            for (int i = 0; i < 130 / JUMP_SPEED; i++) {
+                y = y - JUMP_SPEED;
+                setLocation(x, y);
+                try {
+                    Thread.sleep(5);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            up = false;
+            down();
+        }).start();
+    }
+
+    @Override
+    public void down() {
+        down = true;
+        new Thread(() -> {
+            while (down) {
+                y = y + JUMP_SPEED;
+                setLocation(x, y);
+                try {
+                    Thread.sleep(3);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            down = false;
+        }).start();
+    }
+
+    @Override
+    public void left() {
+        left = true;
+        playerWay = PlayerWay.LEFT;
+        new Thread(() -> {
+            System.out.println("left start");
+            while(left) {
+                setIcon(playerL);
+                x = x - SPEED;
+                setLocation(x, y);
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+    @Override
+    public void right() {
+        right = true;
+        playerWay = PlayerWay.RIGHT;
+        new Thread(() -> {
+            System.out.println("right start");
+            while(right) {
+                setIcon(playerR);
+                x = x + SPEED;
+                setLocation(x, y);
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+}
